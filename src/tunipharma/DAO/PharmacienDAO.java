29e042c4ed02ea51d53902311edeb15434b167ca
EmnaZayani <emna.zayani@esprit.tn>
2014-02-21 00:42:26 +0100

    adding EnvoyerSMS() to PharmacienDAO

---
+++ src/tunipharma/DAO/PharmacienDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tunipharma.DAO;
+
+import tunipharma.entities.Pharmacie;
+import tunipharma.entities.Pharmacien;
+
+/**
+ *
+ * @author Emna
+ */
+public class PharmacienDAO {
+    
+    public void InscriptionPharmacien()
+            
+    {
+        
+    }
+    
+    public void EnvoyerSMS(int numero,Pharmacien pharmacien, Pharmacie pharmacie)
+    {
+        
+    }
+    
+}
